76d521f0ce182c97562fd34d53a119391269ae74
zaniphrom <brian.reltub@gmail.com>
2014-04-27 18:47:11 -0300

    updated

--- systemSetup.sh
+++ systemSetup.sh
@@ -4,7 +4,6 @@
 # To download this script
 # sudo wget -c https://github.com/zaniphrom/bash/blob/master/systemSetup.sh && chmod 775 systemSetup.sh
 
-echo "just print as test"
 echo -e "enter the user pw\n:> "
 read PASSWD
 
@@ -12,7 +11,10 @@ read PASSWD
 sudo apt-get -y update
 sudo apt-get -y dist-upgrade
 sudo apt-get -y install python python-pip
-sudo apt-get -y install beautifulsoup4
+sudo apt-get -y install unzip
+sudo pip install beautifulsoup4
+
+
 
 # make new user with sudo rights and home directory
 useradd -m -s /bin/bash -d /home/brian -U brian -p $PASSWD
