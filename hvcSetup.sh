968ec5e0a65f0e70d7c858126959f74b5e1024e2
zaniphrom <brian.reltub@gmail.com>
2014-04-27 19:41:01 -0300

    HeavyCoin mining setup script
    1gh pool mining direct

--- hvcSetup.sh
+++ hvcSetup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-# A script to get HVC mining quickly
-# sudo wget -c https://github.com/zaniphrom/bash/blob/master/hvcSetup.sh && chmod 775 hvcSetup.sh
+#A script to get HVC mining quickly
+#sudo wget -c https://github.com/zaniphrom/bash/blob/master/hvcSetup.sh && chmod 775 hvcSetup.sh
+
 sudo dd if=/dev/zero of=/swapfile bs=64M count=16 
 sudo mkswap /swapfile
 sudo swapon /swapfile
-sudo apt-get update
-sudo apt-get dist-upgrade
-sudo apt-get install automake build-essential libcurl4-openssl-dev git zip
+sudo apt-get -y update
+sudo apt-get -y dist-upgrade
+sudo apt-get -y install automake build-essential libcurl4-openssl-dev git zip
 git clone https://github.com/heavycoin/cpuminer-heavycoin
 cd cpuminer-heavycoin
 sudo sed -i 's/@LIBCURL@ @JANSSON_LIBS@ @PTHREAD_LIBS@ @WS2_LIBS@ -lssl/@LIBCURL@ @JANSSON_LIBS@ @PTHREAD_LIBS@ @WS2_LIBS@ -lssl -lcrypto/' Makefile.am
