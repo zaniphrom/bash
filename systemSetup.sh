4d2f2d4985137abfe8b6e12e2ccb3fa00823bbb2
zaniphrom <brian.reltub@gmail.com>
2014-04-27 18:41:13 -0300

    update

--- systemSetup.sh
+++ systemSetup.sh
@@ -2,13 +2,11 @@
 
 # A script to update a new VM to my usual needs
 # To download this script
-# wget -c https://github.com/zaniphrom/bash/blob/master/systemSetup.sh && chmod 775 systemSetup.sh
- 
+# sudo wget -c https://github.com/zaniphrom/bash/blob/master/systemSetup.sh && chmod 775 systemSetup.sh
 
 echo "just print as test"
 echo -e "enter the user pw\n:> "
 read PASSWD
-  
 
 # Usual update
 sudo apt-get -y update
@@ -16,9 +14,7 @@ sudo apt-get -y dist-upgrade
 sudo apt-get -y install python python-pip
 sudo apt-get -y install beautifulsoup4
 
-
 # make new user with sudo rights and home directory
-
 useradd -m -s /bin/bash -d /home/brian -U brian -p $PASSWD
 
 
