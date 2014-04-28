19dab03790851f8b06da11e52be5a04d28eccc63
zaniphrom <brian.reltub@gmail.com>
2014-04-27 21:01:09 -0300

    Ghost Setup Script

--- ghostSetup.sh
+++ ghostSetup.sh
@@ -3,7 +3,7 @@
 # To download this script
 # sudo wget -c https://raw.githubusercontent.com/zaniphrom/bash/master/ghostSetup.sh && chmod 775 ghostSetup.sh
 
-echo -e "This script will install Python, Node.Js, Ghost and nginx.\n Do you wish to proceed? [y|n]"
+echo -e "This script will install Python, Node.Js, Ghost, forever and nginx.\n Do you wish to proceed? [y|n]"
 read ANS
 [ $ANS == "n" ] && echo "exiting....." && sleep 1 && exit 0 || echo "programme running"
 
@@ -21,7 +21,7 @@ echo "User created"
 cd /home/$USERNAME
 # Base updates and installs
 sudo apt-get -y install python-software-properties 
-sudo apt-add-repository ppa:chris-lea/node.js
+sudo apt-add-repository ppa:chris-lea/node.js -y
 sudo apt-get -y update
 sudo apt-get -y dist-upgrade
 sudo apt-get -y install nodejs
@@ -29,11 +29,10 @@ sudo npm install forever -g
 sudo apt-get -y install unzip
 sudo wget https://ghost.org/zip/ghost-latest.zip
 sudo unzip -d ghost ghost-latest.zip
-sudo chown -R www-data:www-data ghost
 cd ghost/
 sudo npm install --production
-sudo apt-get -y nginx
-sudo pip install beautifulsoup4
+sudo chown -R www-data:www-data ghost
+sudo apt-get -y install nginx
 clear
 echo "Ghost, nginx, and nodejs are installed"
 echo "You need to configure them now"
